2d79010a7ff4a1a92872e9ae72bd96fe5f13b74a
karthik01932 <113256802+karthik01932@users.noreply.github.com>
2023-10-11 18:45:02 +0530

    scale app

---
+++ scale.js
@@ -0,0 +1,23 @@
+function addData(){
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var number = document.getElementById('number').value;
+
+    localStorage.setItem('username',name);
+    localStorage.setItem('useremail',email);
+    localStorage.setItem('usernumber',number);
+}
+const form = document.querySelector('#form');
+const username = document.querySelector('#name');
+const useremail = document.querySelector('#email');
+const usernumber = document.querySelector('#number');
+const userrecords = document.querySelector('#users');
+
+form.addEventListener('submit', onsubmit);
+function onsubmit(e){
+    e.preventDefault();
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${username.value} - ${useremail.value} - ${usernumber.value}`));
+    userrecords.appendChild(li);
+}
+
