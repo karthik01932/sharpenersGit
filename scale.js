00d6d93ad774dbf35e447371e950aa557d839b8b
karthik01932 <113256802+karthik01932@users.noreply.github.com>
2023-10-12 14:42:44 +0530

    edit function

--- scale.js
+++ scale.js
@@ -17,17 +17,30 @@ form.addEventListener('submit', onsubmit);
 function onsubmit(e){
     e.preventDefault();
     const li = document.createElement('li');
-    li.appendChild(document.createTextNode(`${username.value} - ${useremail.value} - ${usernumber.value}`));
+    var litext = document.createTextNode(`${username.value} - ${useremail.value} - ${usernumber.value}`);
+    li.appendChild(litext);
     userrecords.appendChild(li);
 
+    // delete
     const deleteButton = document.createElement('input');
     deleteButton.type = 'button';
     deleteButton.value = 'Delete';
     deleteButton.onclick = () => {
-        // localStorage.removeItem();
+        localStorage.removeItem(litext);
         userrecords.removeChild(li)
     }
     li.appendChild(deleteButton);
+    userrecords.appendChild(li);
+
+    // edit
+    const editButton = document.createElement('input');
+    editButton.type = 'button';
+    editButton.value = 'Edit';
+    editButton.onclick = () => {
+        localStorage.removeItem(litext);
+        userrecords.removeChild(li)
+    }   
+    li.appendChild(editButton);
     userrecords.appendChild(li);
 }
 
