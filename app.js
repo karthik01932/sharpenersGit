82b4919835934d165b85390ca3fe7232e6a54544
karthik01932 <113256802+karthik01932@users.noreply.github.com>
2024-01-11 10:30:09 +0530

    routes and filters

--- app.js
+++ app.js
@@ -3,26 +3,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
-});
-// use
-// get
-// post
-// put
-// delete
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
-app.use('/',(req,res,next)=>{
-    res.send('<h1> Hello from express</h1>')
-});
-// const server = http.createServer(app);
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
-// server.listen(4000);
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
+})
+
 app.listen(4000);
 
 
