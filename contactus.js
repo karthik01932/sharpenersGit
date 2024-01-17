dbcb74e9228ce32935e1ee65ece769612df8fc84
karthik01932 <113256802+karthik01932@users.noreply.github.com>
2024-01-17 20:23:36 +0530

    contact and success

--- contactus.js
+++ contactus.js
@@ -6,15 +6,15 @@ const router = express.Router();
 
 router.get('/contactus' ,(req,res,next)=>{
     res.sendFile(path.join(rootDir,'views','contactus.html'));
-    console.log(res.body);
+    // console.log(res.body);
 })
-router.post('/success',(req,res,next)=>{
+router.post('/contactus',(req,res,next)=>{
     console.log(req.body);
-    res.write('<html>');
-    res.write('<head><title>Form successfuly filled</title></head>');
-    res.write('<body><h1>Form successfuly filled</h1></body>');
-    res.write('</html>');
-    // res.redirect('/');
+    // res.write('<html>');
+    // res.write('<head><title>Form successfuly filled</title></head>');
+    // res.write('<body><h1>Form successfuly filled</h1></body>');
+    // res.write('</html>');
+    res.redirect('/success');
     
 })
 
